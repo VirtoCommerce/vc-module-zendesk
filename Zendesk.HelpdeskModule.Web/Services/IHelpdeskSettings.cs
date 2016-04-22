b5eb84c7904ecb8a35606c67aa04445e72204da2
artem-dudarev <ad@virtoway.com>
2016-04-22 16:12:27 +0200

    merge

---
+++ Zendesk.HelpdeskModule.Web/Services/IHelpdeskSettings.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Zendesk.HelpdeskModule.Web.Services
+{
+    public interface IHelpdeskSettings
+    {
+        string AccessToken { get; set; }
+        string Subdomain { get; }
+        string CustomerEmail { get; }
+    }
+}
